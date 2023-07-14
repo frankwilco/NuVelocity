66c60cfffc3a7c3dc1832b250b5ce1584bb50e58
Francis Dominic Fajardo <fajardo@duck.com>
2023-07-15 01:43:04 +0800

    Parse zlib-compressed sprite sheets in Sequence
    This also fixes a few incorrect assumptions about the first byte flag and embedded lists (the sequence's original properties are sometimes embedded, aside from the frame info).

--- src/Sequence.cs
+++ src/Sequence.cs
@@ -1,18 +1,22 @@
 ﻿using ICSharpCode.SharpZipLib.Zip.Compression;
-using System.Xml.Linq;
 
 namespace Velocity
 {
     public class Sequence
     {
-        private const byte kFlagZlibCompression = 0x01;
-        private const byte kFlagNoCompression = 0x55;
+        private const byte kFlagStandard = 0x01;
+        private const byte kFlagHighDefinition = 0x55;
 
         public int Flags { get; private set; }
 
-        private byte[] _frameInfo;
-        private byte[] _sequenceSpriteSheet;
-        private byte[] _maskData;
+        public bool IsCompressed { get; private set; }
+
+        internal byte[] _embeddedLists;
+        internal byte[] _sequenceSpriteSheet;
+        internal byte[] _rawMaskData;
+
+        public int Width { get; private set; }
+        public int Height { get; private set; }
 
         public Sequence(Stream stream)
         {
@@ -20,9 +24,9 @@ namespace Velocity
             Flags = reader.ReadByte();
             switch (Flags)
             {
-                case kFlagZlibCompression:
+                case kFlagStandard:
                     break;
-                case kFlagNoCompression:
+                case kFlagHighDefinition:
                     throw new NotImplementedException();
                 default:
                     throw new InvalidDataException();
@@ -33,39 +37,70 @@ namespace Velocity
             int frameInfoInflatedSize = reader.ReadInt32();
             byte[] rawFrameInfo = reader.ReadBytes(frameInfoDeflatedSize);
             inflater.SetInput(rawFrameInfo);
-            _frameInfo = new byte[frameInfoInflatedSize];
-            if (inflater.Inflate(_frameInfo) != frameInfoInflatedSize)
+            _embeddedLists = new byte[frameInfoInflatedSize];
+            if (inflater.Inflate(_embeddedLists) != frameInfoInflatedSize)
             {
                 throw new InvalidDataException();
             }
             inflater.Reset();
 
-            reader.ReadByte(); // 1-byte padding.
-            int imageSize = reader.ReadInt32();
-            _sequenceSpriteSheet = reader.ReadBytes(imageSize);
-
-            reader.ReadByte(); // 1-byte padding.
-            int maskInflatedSize = reader.ReadInt32();
-            long distanceToEof = stream.Length - stream.Position;
-            byte[] rawMaskData = reader.ReadBytes((int)distanceToEof);
-            inflater.SetInput(rawMaskData);
-            _maskData = new byte[maskInflatedSize];
-            if (inflater.Inflate(_maskData) != maskInflatedSize)
+            if (reader.PeekChar() == -1)
             {
-                throw new InvalidDataException();
+                // No sprite sheet data. This is probably an empty sequence.
+                return;
+            }
+
+            IsCompressed = reader.ReadBoolean();
+            if (IsCompressed)
+            {
+                byte unknown1 = reader.ReadByte(); // unknown value
+                int imageDeflatedSize = reader.ReadInt32();
+                int imageInflatedSize = reader.ReadInt32();
+                _sequenceSpriteSheet = new byte[imageInflatedSize];
+                inflater.SetInput(reader.ReadBytes(imageDeflatedSize));
+                if (inflater.Inflate(_sequenceSpriteSheet) != imageInflatedSize)
+                {
+                    throw new InvalidDataException();
+                }
+                Width = reader.ReadInt32();
+                Height = reader.ReadInt32();
+            }
+            else
+            {
+                int imageSize = reader.ReadInt32();
+                _sequenceSpriteSheet = reader.ReadBytes(imageSize);
+
+                reader.ReadByte(); // 1-byte padding.
+                int maskInflatedSize = reader.ReadInt32();
+                long distanceToEof = stream.Length - stream.Position;
+                byte[] rawMaskData = reader.ReadBytes((int)distanceToEof);
+                inflater.SetInput(rawMaskData);
+                _rawMaskData = new byte[maskInflatedSize];
+                if (inflater.Inflate(_rawMaskData) != maskInflatedSize)
+                {
+                    throw new InvalidDataException();
+                }
             }
         }
 
         public Image ToImage()
         {
-            Image<Rgba32> image = Image.Load<Rgba32>(new ReadOnlySpan<byte>(_sequenceSpriteSheet));
+            if (_sequenceSpriteSheet == null)
+            {
+                return null;
+            }
 
-            byte[] temp = new byte[_maskData.Length];
-            _maskData.CopyTo(temp, 0);
-            byte[] componentData = new byte[image.Width * image.Height];
-            FrameUtils.ParseComponent(0, temp, componentData, image.Width, image.Height);
-            image.ProcessPixelRows(FrameUtils.MaskFromByteArray(componentData));
-
+            Image<Rgba32> image;
+            if (IsCompressed)
+            {
+                image = FrameUtils.LoadLayeredRgbaImage(_sequenceSpriteSheet, Width, Height);
+            }
+            else
+            {
+                image = FrameUtils.LoadJpegImage(_sequenceSpriteSheet, _rawMaskData);
+                Width = image.Width;
+                Height = image.Height;
+            }
             return image;
         }
 
