7a61b40978ece8edb3c7eeb45439b25763d80a6e
Francis Dominic Fajardo <fajardo@duck.com>
2024-07-09 16:00:18 +0800

    Implement Font and FontBitmap property lists

---
+++ src/Graphics/Font.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace NuVelocity.Graphics;
+
+// TODO: check default values.
+[PropertyRoot("CFont", "Font")]
+public class Font
+{
+    [Property("Font Family", defaultValue: "Resources/Fonts/TRUE TYPES/!default.ttf")]
+    public string? FontFamily { get; set; }
+
+    [Property("Blit Type", defaultValue: Graphics.BlitType.TransparentMask)]
+    public BlitType? BlitType { get; set; }
+
+    // FIXME: colors are not handled by serialization code.
+    [Property("Generated Color")]
+    public Color? GeneratedColor { get; set; }
+
+    [Property("Point Size", defaultValue: 12)]
+    public int? PointSize { get; set; }
+
+    [Property("Generate All Caps", defaultValue: false)]
+    public bool? GenerateAllCaps { get; set; }
+}
---
+++ src/Graphics/FontBitmap.cs
@@ -0,0 +1,24 @@
+﻿namespace NuVelocity.Graphics;
+
+// TODO: check default values.
+[PropertyRoot("CFontBitmap", "Bitmap Font")]
+public class FontBitmap : Font
+{
+    [Property("First ASCII", defaultValue: 0)]
+    public int? FirstAscii { get; set; }
+
+    [Property("Last ASCII", defaultValue: 0)]
+    public int? LastAscii { get; set; }
+
+    [Property("Fixed Width", defaultValue: false)]
+    public bool? IsFixedWidth { get; set; }
+
+    [Property("Height Without Decenders")]
+    internal int? HeightWithoutDecenders
+    {
+        get => XHeight;
+        set => XHeight = value;
+    }
+
+    public int? XHeight { get; set; }
+}
